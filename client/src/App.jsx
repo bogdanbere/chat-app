c9b51f52d1e0c2d01970a65db28d684ed20d8694
bogdanbere <bogdanbere@gmail.com>
2025-01-04 16:23:05 +0200

    Added header, footer, adjusted header and footer to not show on 404 and added laoding component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,52 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, useLocation, Navigate } from "react-router-dom";
 import { ToastContainer, Bounce } from "react-toastify";
 import { setUser } from "./store/userReducer";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
+import MePage from "./pages/MePage";
+import ProfilePage from "./pages/ProfilePage";
 import PageNotFound from "./pages/PageNotFound";
+import Loading from "./components/Loading";
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const dispatch = useDispatch();
+  const location = useLocation();
   const theme = useSelector((state) => state.theme);
   const user = useSelector((state) => state.user);
   useEffect(() => {
-    dispatch(setUser());
+    const fetchUser = async () => {
+      await dispatch(setUser());
+      setIsLoading(false);
+    };
+
+    fetchUser();
   }, [dispatch]);
 
   return (
     <div data-theme={theme}>
+      {location.pathname === "/404" ? null : <Navbar />}
       <Routes>
-        <Route path="/" element={user ? <HomePage /> : <LoginPage />} />
+        <Route
+          path="/"
+          element={
+            isLoading ? <Loading /> : user ? <HomePage /> : <LoginPage />
+          }
+        />
         <Route path="/login" element={!user ? <LoginPage /> : <HomePage />} />
         <Route path="/signup" element={!user ? <SignupPage /> : <HomePage />} />
-        <Route path="*" element={<PageNotFound />} />
+        <Route path="/profile/me" element={user ? <MePage /> : <LoginPage />} />
+        <Route
+          path="/profile/:id"
+          element={user ? <ProfilePage /> : <LoginPage />}
+        />
+        <Route path="*" element={<Navigate to="/404" />} />
+        <Route path="/404" element={<PageNotFound />} />
       </Routes>
 
       <ToastContainer
@@ -38,6 +62,7 @@ const App = () => {
         theme={theme}
         transition={Bounce}
       />
+      {location.pathname === "/404" ? null : <Footer />}
     </div>
   );
 };
